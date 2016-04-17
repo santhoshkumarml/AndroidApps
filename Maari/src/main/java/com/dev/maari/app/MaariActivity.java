3cb2058191392c8b095430ee94fa125e3fdb9080
Santhosh Kumar Manavasi Lakshminarayanan <santhosh@streamsets.com>
2016-04-16 22:32:31 -0700

    Changs

--- Maari/src/main/java/com/dev/maari/app/MaariActivity.java
+++ Maari/src/main/java/com/dev/maari/app/MaariActivity.java
@@ -12,13 +12,13 @@ public class MaariActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_route_finder);
+    setContentView(R.layout.activity_maari);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.route_finder, menu);
+    getMenuInflater().inflate(R.menu.maari, menu);
     return true;
   }
 
