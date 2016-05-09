a257d1eadf3fbeeaa8e66ffb5ae903885544a71e
Santhosh Kumar Manavasi Lakshminarayanan <santhosh@streamsets.com>
2016-05-08 17:01:02 -0700

    Refactoring classes

--- Maari/src/main/java/com/dev/maari/model/TransactionLogInfo.java
+++ Maari/src/main/java/com/dev/maari/model/TransactionLogInfo.java
@@ -1,22 +1,4 @@
-/**
- * Copyright 2016 StreamSets Inc.
- * <p>
- * Licensed under the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.dev.maari.model;
 
 import com.j256.ormlite.field.DatabaseField;
@@ -27,7 +9,7 @@ import java.sql.Time;
 
 @DatabaseTable(tableName = "AgentTransactionLog")
 public class TransactionLogInfo implements Serializable{
-  @DatabaseField(id = true)
+  @DatabaseField(id = true, generatedId = true)
   long transactionLogId=0;
 
   @DatabaseField
@@ -55,4 +37,15 @@ public class TransactionLogInfo implements Serializable{
     return time;
   }
 
+  public void setAmount(long amount) {
+    this.amount = amount;
+  }
+
+  public void setActorId(String actorId) {
+    this.actorId = actorId;
+  }
+
+  public void setTime(Time time) {
+    this.time = time;
+  }
 }
