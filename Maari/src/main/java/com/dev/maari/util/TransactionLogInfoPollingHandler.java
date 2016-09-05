67fa6edc45bdb47daa8f6fa40068f8d68ea984da
Santhosh Kumar Manavasi Lakshminarayanan <santhosh@streamsets.com>
2016-09-05 14:13:33 -0700

    Changes

--- Maari/src/main/java/com/dev/maari/util/TransactionLogInfoPollingHandler.java
+++ Maari/src/main/java/com/dev/maari/util/TransactionLogInfoPollingHandler.java
@@ -2,6 +2,6 @@ package com.dev.maari.util;
 
 import com.dev.maari.model.TransactionLogInfo;
 
-interface  TransactionLogInfoPollingHandler {
+public interface TransactionLogInfoPollingHandler {
   boolean handleTransactionLogPollingAction(TransactionLogInfo logInfo);
 }
