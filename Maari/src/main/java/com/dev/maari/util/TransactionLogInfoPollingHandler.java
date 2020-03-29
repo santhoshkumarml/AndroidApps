6baf0942a4ceac877edf5c496072ec0a25e77996
santhoshkumarml <santhosh@streamsets.com>
2020-03-29 11:14:28 -0700

    Fix master build

--- Maari/src/main/java/com/dev/maari/util/TransactionLogInfoPollingHandler.java
+++
@@ -1,7 +0,0 @@
-package com.dev.maari.util;
-
-import com.dev.maari.model.TransactionLogInfo;
-
-public interface TransactionLogInfoPollingHandler {
-  boolean handleTransactionLogPollingAction(TransactionLogInfo logInfo);
-}
